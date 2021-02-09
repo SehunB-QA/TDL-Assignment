b1c07bd8c8dc766f37259aa6757eaf78591d41c6
SehunB-QA <sbabatunde@qa.com>
2021-02-09 18:44:07 +0000

    SQTDLWA-18 : Produded the backend prokect. Added starting base classes for the TDL backend to fill in :)

---
+++ TDLAssignmentBackend/src/main/java/com/qa/tdlassignmentbackend/config/AppConfig.java
@@ -0,0 +1,5 @@
+package com.qa.tdlassignmentbackend.config;
+
+public class AppConfig {
+
+}
---
+++ TDLAssignmentBackend/src/main/java/com/qa/tdlassignmentbackend/persistence/repo/ItemsRepo.java
@@ -0,0 +1,5 @@
+package com.qa.tdlassignmentbackend.persistence.repo;
+
+public interface ItemsRepo {
+
+}
