8d1e327d16a82d97231c42f70c489bf7c53c95d4
SehunB-QA <sbabatunde@qa.com>
2021-02-10 18:29:44 +0000

    SQTDLWA-24 & SQTDLWA-25 : Added TasksDTO & ToDoListDAO

--- TDLAssignmentBackend/src/main/java/com/qa/tdlassignmentbackend/dtos/TasksDTO.java
+++ TDLAssignmentBackend/src/main/java/com/qa/tdlassignmentbackend/dtos/TasksDTO.java
@@ -1,5 +1,25 @@
 package com.qa.tdlassignmentbackend.dtos;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//Lombok to automatically generate :) our constructors, ToString, getters & setters etc... 
+@Data
+@NoArgsConstructor 
 public class TasksDTO {
+	
+	private Long taskID;
+	
+	private String name;
+	private String description;
+    private String colour;
+
+
+public TasksDTO(String name, String description, String colour)
+{
+	this.name = name;
+	this.description = description;
+	this.colour = colour;
+}
 
 }
--- TDLAssignmentBackend/src/main/java/com/qa/tdlassignmentbackend/dtos/ToDoListDTO.java
+++ TDLAssignmentBackend/src/main/java/com/qa/tdlassignmentbackend/dtos/ToDoListDTO.java
@@ -1,5 +1,18 @@
 package com.qa.tdlassignmentbackend.dtos;
 
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//Lombok to automatically generate :) our constructors, ToString, getters & setters etc... 
+@Data
+@NoArgsConstructor 
 public class ToDoListDTO {
+	
+	private Long listID;
+	private String name;
+	
+	private List<TasksDTO> tasks = new ArrayList<>();
 
 }
