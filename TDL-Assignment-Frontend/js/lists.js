3fa2702ed3fbb9187d320d262e79109b7e9b2794
SehunB-QA <sbabatunde@qa.com>
2021-02-16 20:35:01 +0000

    SQTDLWA-46 : Added icons, colour styling. Started working on implementing list crud functions

---
+++ TDL-Assignment-Frontend/js/lists.js
@@ -0,0 +1,26 @@
+'use strict'
+
+
+// To Do List
+
+//Create
+function createList()
+{
+    const url = ""
+
+    let dataToSend = {
+        name: 
+    }
+
+    let request = new Request(url, {
+        method: "POST",
+        body: dataToSend,
+        headers: new Headers()
+    });
+
+    fetch(request)
+        .then(function () {
+        
+    })
+
+}
