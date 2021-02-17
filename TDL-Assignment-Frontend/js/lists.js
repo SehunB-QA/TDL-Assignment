cc543ce1acf9892134445d7a201a56ed3b531fca
SehunB-QA <sbabatunde@qa.com>
2021-02-17 11:03:05 +0000

    SQTDLWA-69 & SQTDLWA-37 : The input string wil remove white spaces and change the input to lowercase

--- TDL-Assignment-Frontend/js/lists.js
+++ TDL-Assignment-Frontend/js/lists.js
@@ -33,8 +33,13 @@ function createList()
     let listName = document.querySelector("#listnameInput").value;
     const url = "http://localhost:9094/todolist/create";
 
+    //Turn all of the input to lowercase
+    let finalListNameString = listName.toLowerCase();
+    //Remove all whitespaces/ spacing from input string
+    finalListNameString = finalListNameString.replace(/\s+/g, '');
+
     let dataToSend = {
-        name: listName
+        name: finalListNameString
     }
 
     let request = new Request(url, {
@@ -50,12 +55,12 @@ function createList()
         //Response from API
         console.log("List created!");
          //Display a sucess message on page if it's gone through :)
-        let sucessText = document.querySelector("#createdListSuccessText").innerHTML = "List Created!";
+        let successText = document.querySelector("#createdListSuccessText").innerHTML = "List Created!";
     }) 
     //Display error on page if there's a failure
     .catch((error) => {
         console.log(`Request failed ${error}`);
-        let sucessText = document.querySelector("#failedCreatedListText").innerHTML = `Failed to create list:  ${error}`;
+        let failedText = document.querySelector("#failedCreatedListText").innerHTML = `Failed to create list:  ${error}`;
 
       });
 
