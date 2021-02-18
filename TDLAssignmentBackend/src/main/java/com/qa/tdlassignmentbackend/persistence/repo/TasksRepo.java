64e6cdffa190493033ec14c4040f324c201bd166
SehunB-QA <sbabatunde@qa.com>
2021-02-18 12:43:19 +0000

    SQTDLWA-69 & SQTDLWA-38 & SQTDLWA-18 : Refactored custom read queries to optionals from the incorrect list type. Added a readybylist function on web end. Fixed title and other things

--- TDLAssignmentBackend/src/main/java/com/qa/tdlassignmentbackend/persistence/repo/TasksRepo.java
+++ TDLAssignmentBackend/src/main/java/com/qa/tdlassignmentbackend/persistence/repo/TasksRepo.java
@@ -1,6 +1,6 @@
 package com.qa.tdlassignmentbackend.persistence.repo;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -21,10 +21,10 @@ public interface TasksRepo extends JpaRepository<Tasks,Long> {
 	//Custom Queries 
 	
 	@Query(value ="SELECT * FROM tasks WHERE COLOUR=?1", nativeQuery = true)
-	List<Tasks> findByColourCode(String colour);
+	 Optional <Tasks> findByColourCode(String colour);
 	
 	@Query(value ="SELECT * FROM tasks WHERE NAME=?1", nativeQuery = true)
-	List<Tasks> findByName(String name);
+	Optional <Tasks> findByName(String name);
 	
 	@Modifying 
 	@Transactional 
